7485709e63f9dab31cc7b480431ce2e576e8c50c
willyw0nka <simon.albert75@gmail.com>
2024-04-10 15:32:21 +0200

    fix: dont bound TypeVar T

--- pygrype/core/decorators/to_json.py
+++ pygrype/core/decorators/to_json.py
@@ -2,7 +2,7 @@ import json
 from dataclasses import asdict
 from typing import Type, TypeVar
 
-T = TypeVar('T', bound='DataClass')
+T = TypeVar('T')
 
 def to_json(cls: Type[T]) -> Type[T]:
     def to_json_func(self: T):
