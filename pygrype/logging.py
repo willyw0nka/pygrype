641ceb9f3ea7d25c9b6eb1914855dc1eee630647
willyw0nka <simon.albert75@gmail.com>
2024-04-16 11:29:46 +0200

    fix: update logging

---
+++ pygrype/logging.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
