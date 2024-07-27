be4b715702502732894ef7e1a8e9e5ddbbe15dcd
Rob Berwick <rob.berwick@gmail.com>
2024-07-27 17:15:21 +0100

    Do logging setup at module level
    Do the logger setup work at the module level, and have `get_logger` just return the instance. This means that `get_logger` can be called elsewhere without duplicating that setup.

--- pygrype/logging.py
+++ pygrype/logging.py
@@ -1,14 +1,15 @@
 import logging
 import sys
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 def get_logger() -> logging.Logger:
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
     return logger
